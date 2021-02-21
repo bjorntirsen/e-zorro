036433e20ba9f9681706688faaf1343d2b1042f8
Björn Tirsén <bjorntirsen@gmail.com>
2021-02-21 11:24:10 +0100

    ✨Add routes and imports to landing page

--- src/App.js
+++ src/App.js
@@ -1,12 +1,55 @@
 import React from 'react'
-import logo from './logo.svg';
+import { Route, Switch } from 'react-router-dom';
 import './App.css';
+
+import Navbar from './components/Navbar'
+
+import CryptoPage from './pages/CryptoPage';
+import CurrenciesDetailPage from './pages/CurrenciesDetailPage';
+import CurrenciesPage from './pages/CurrenciesPage';
+import IndexesPage from './pages/IndexesPage';
+import LandingPage from './pages/LandingPage';
+import MarketPage from './pages/MarketPage';
+import MarketDetailPage from './pages/MarketDetailPage';
+import InstrumentPage from './pages/InstrumentPage';
+import CryptoDetailPage from './pages/CryptoDetailPage'
+import IndexDetailPage from './pages/IndexDetailPage'
 
 function App() {
   return (
     <div>
-      <p>App.js</p>
-      
+      <Navbar />
+      <Switch>
+        <Route path="/currencies/:id" component={CurrenciesDetailPage} />
+
+        <Route path="/currencies">
+          <CurrenciesPage />
+        </Route>
+
+        <Route path="/CryptoDetailPage/:id" component={CryptoDetailPage} />
+
+        <Route path="/crypto">
+          <CryptoPage />
+        </Route>
+
+        <Route path="/Indexes/:id" component={IndexDetailPage}/>
+        
+        <Route path="/indexes">
+          <IndexesPage />
+        </Route>
+
+        <Route path="/markets/:marketname/:instrument" component={InstrumentPage} />
+        
+        <Route path="/markets/:marketname" component={MarketDetailPage} />
+        
+        <Route path="/markets">
+          <MarketPage />
+        </Route>
+
+        <Route path="/">
+          <LandingPage />
+        </Route>
+      </Switch>
     </div>
   );
 }
--- src/pages/LandingPage.jsx
+++ src/pages/LandingPage.jsx
@@ -1,6 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import intro from './audio/intro.mp3'
+import intro from '../audio/intro.mp3'
+import zorrow320 from '../images/zorro-w320.png'
+import cryptow538 from '../images/crypto-w538.jpg'
+import currenciesw538 from '../images/currencies-w538.jpg'
+import indexesw538 from '../images/indexes-w538.jpg'
+import marketsw538 from '../images/markets-w538.jpg'
+
 
 export default function LandingPage() {
     return (
@@ -9,7 +15,7 @@ export default function LandingPage() {
                 <div className="col-md-12">
                     <div className="d-flex align-items-center justify-content-center my-headline">
                         <h1 className="text-center font-weight-bold"><small className="text-muted align-self-start">Welcome to</small><br /><span className="display-4 my-headline__brand">eZorro!</span></h1>
-                        <img className="img-fluid w-25 p-3" src="./images/zorro-w320.png" alt=""/>
+                        <img className="img-fluid w-25 p-3" src={zorrow320} alt=""/>
                     </div>
                     <div className="d-flex align-items-center justify-content-center">
                         <audio
@@ -23,7 +29,7 @@ export default function LandingPage() {
             <div className="row">
                 <div className="col-md-6">
                     <div className="card my-3">
-                        <img className="img-fluid" src="./images/crypto-w538.jpg" alt=""/>
+                        <img className="img-fluid" src={cryptow538} alt=""/>
                         <div className="card-body">
                             <Link to="/crypto" className="btn btn-primary btn-block btn-lg">Crypto</Link>
                         </div>
@@ -31,7 +37,7 @@ export default function LandingPage() {
                 </div>
                 <div className="col-md-6 my-3">
                     <div className="card">
-                        <img className="img-fluid" src="./images/currencies-w538.jpg" alt=""/>
+                        <img className="img-fluid" src={currenciesw538} alt=""/>
                         <div className="card-body">
                             <Link to="/currencies" className="btn btn-primary btn-block btn-lg">Currencies</Link>
                         </div>
@@ -39,7 +45,7 @@ export default function LandingPage() {
                 </div>
                 <div className="col-md-6 my-3">
                     <div className="card">
-                        <img className="img-fluid" src="./images/indexes-w538.jpg" alt=""/>
+                        <img className="img-fluid" src={indexesw538} alt=""/>
                         <div className="card-body">
                             <Link to="/indexes" className="btn btn-primary btn-block btn-lg">Indexes</Link>
                         </div>
@@ -47,7 +53,7 @@ export default function LandingPage() {
                 </div>
                 <div className="col-md-6 my-3">
                     <div className="card">
-                        <img className="img-fluid" src="./images/markets-w538.jpg" alt=""/>
+                        <img className="img-fluid" src={marketsw538} alt=""/>
                         <div className="card-body">
                             <Link to="/markets" className="btn btn-primary btn-block btn-lg">Markets</Link>
                         </div>

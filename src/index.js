7e1057633210d1cc2ee48992c0f3155cf4200241
Björn Tirsén <bjorntirsen@gmail.com>
2021-02-21 11:03:02 +0100

    ✨Add custom SCSS to Bootstrap

--- src/index.js
+++ src/index.js
@@ -4,6 +4,7 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { BrowserRouter } from 'react-router-dom';
+import './styles/CurrencyStyles.scss'
 
 ReactDOM.render(
   <React.StrictMode>
